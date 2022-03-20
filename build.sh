7f38235e1be35e7aeec23334ecf4f22a45a9fd13
mike <michael.v.mccarron@gmail.com>
2022-03-20 16:10:21 -0600

    Wip

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
-docker build -t mimc/alpine-dev:1.0 .
+cmd='podman'
+TUID=1000
+TGID=1000
+TUNAME=mike
+TGNAME=gamers
+VERSION=1.0
+
+# Replace "-v /local/path:/container/path" with the paths that should be mounted in the container (multiple -v's can be added)
+
+$cmd build --build-arg UID=$TUID --build-arg GID=$TGID --build-arg UNAME=$TUNAME --build-arg GNAME=$TGNAME -t mimc/alpine-dev:$VERSION .
 if [ "$?" == "0" ]; then
-  IMAGE_SHA=$(docker create -it --entrypoint=/bin/bash mimc/alpine-dev:1.0)
-  docker start $IMAGE_SHA
-  docker attach $IMAGE_SHA
-  docker container rm $IMAGE_SHA
-  docker image rm mimc/alpine-dev:1.0
+  IMAGE_SHA=$($cmd create -v /local/path:/container/path --privileged -it --entrypoint=/bin/bash mimc/alpine-dev:$VERSION)
+  $cmd start $IMAGE_SHA
+  $cmd attach $IMAGE_SHA
+  # $cmd container rm $IMAGE_SHA
+  # $cmd image rm mimc/alpine-dev:1.0
 fi
 
-# docker create -it --entrypoint=/bin/bash -v /local/path:/mnt --privileged mimc/alpine-dev:1.0
